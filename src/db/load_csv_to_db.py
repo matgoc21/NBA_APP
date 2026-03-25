26ff084ad7ae27c26f9752ea484977b7f35b2164
Mat_Goc <mateuszgocki@gmail.com>
2026-03-25 18:12:28 +0100

    new data

--- src/db/load_csv_to_db.py
+++ src/db/load_csv_to_db.py
@@ -2,25 +2,26 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 DB_URL = "postgresql://nba_user:supertajnehaslo123@localhost:5432/nba_predictions"
-CSV_FILE_PATH = '../../data/raw/player_stats_2023-24.csv'
+FILES_TO_LOAD = {
+    'raw_games': '../../data/raw/games_2023-24.csv',
+    'raw_player_stats': '../../data/raw/player_stats_2023-24.csv'
+}
 
 def load_data():
     print("Łączenie z bazą danych")
     engine = create_engine(DB_URL)
-    print ("Wczytywanie pliku CSV do Pandas...")
-    try:
-        df = pd.read_csv(CSV_FILE_PATH)
-    except FileNotFoundError:
-        print(f"Błąd: Nie znaleziono pliku {CSV_FILE_PATH}")
-        return
+    for table_name, file_path in FILES_TO_LOAD.items():
+        print(f"Wczytywanie pliku {file_path}...")
+        try:
+            df = pd.read_csv(file_path)
 
-    table_name = 'raw_player_stats'
+            print(f"Wysyłanie {len(df)} wierszy do tabel {table_name}...\n")
+            df.to_sql(table_name, engine, if_exists='replace', index=False)
+            print(f"Tabela '{table_name} gotowa. \n")
 
-    print(f"Wysyłanie {len(df)} wierszy do tabeli '{table_name}'...")
-
-    df.to_sql(table_name, engine, if_exists='replace', index=False)
-
-    print("Sukces")
-
+        except FileNotFoundError:
+            print(f"BŁAD: Nie znaleziono pliku {file_path}\n")
+        except Exception as e:
+            print(f"Wystąpił błąd: {e}\n")
 if __name__ == "__main__":
     load_data()
