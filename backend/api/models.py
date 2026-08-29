c6f165f97cf34ac9700066addaebc4e3f544eeab
Mat_Goc <mateuszgocki@gmail.com>
2026-08-29 17:17:37 +0200

    some changes

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique = True)
+    city = models.CharField(max_length=100)
+    abbreviation = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.city} {self.name} ({self.abbreviation})"
+class Player(models.Model):
+    first_name = models.charField(max_length = 100)
+    last_name = models.charField(max_length=100)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='players')
+    position =models.CharField(max_length=20, null = True, blank = True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.team.abbreviation}"
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function App() {
-  const [pts, setPts] = useState(25.5);
-  const [rest, setRest] = useState(1);
-  const [home, setHome] = useState(1);
-  const [prediction, setPrediction] = useState(null);
-  const handleSubmit = async (e) => {
-    e.preventDefault(); // Blokuje domyślne przeładowanie strony po wysłaniu formularza
-    try {
-      const response = await fetch('http://127.0.0.1:8000/api/predict/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          PTS_5G_AVG: parseFloat(pts),
-          DAYS_REST: parseInt(rest),
-          HOME_GAME: parseFloat(home)
-        })
-      });
-      const data = await response.json();
-      setPrediction(data.predicted_pts);
-    } catch(error){
-      console.error("Błąd zapytania:", error);
-
-    }
-  };
+  const [teams, setTeams] = useState([]);
+  const [selectedTeam, setSelectedTeam] = useState("");
+  useEffect(() => {
+    const mockTeams = [
+      { id: 1, name: "Boston Celtics"},
+      { id: 2, name: "Los Angeles Lakers"},
+      { id: 3, name: "Chicago Bulls"},
+      { id: 4, name: "Miami Heat"},
+      { id: 5, name: "Golden State Warriors"},
+    ];
+    setTeams(mockTeams);
+  }, []);
   return (
     <div>
       <h1>NBA stats prediction </h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>PTS (Avarage from 5 games):</label>
-          <input type="number" step= "0.1" value = {pts} onChange = {(e) => setPts(e.target.value)} />
-        </div>
-        <div>
-          <label>Rest days:</label>
-          <input type="number" value={rest} onChange={(e) => setRest(e.target.value)} />
-        </div>
-        <div>
-          <label> Home game: </label>
-          <input type="number" min="0" max="1" value={home} onChange={(e) => setHome(e.target.value)} />
-        </div>
-        <button type="submit">Predict</button>
-      </form>
-      {/* warunkowe renderowanie - div pojawi się tylko, gdy prediction nie jest null */}
-      {prediction !== null && (
-        <h2>Predicted points: {prediction.toFixed(1)}</h2>
+      <h2> Select a team: </h2>
+      <select
+        value={selectedTeam}
+        onChange={(e) => setSelectedTeam(e.target.value)}
+      >
+        <option value ="">--Please select a team --</option>
+        {teams.map((team) => (
+          <option key = {team.id} value = {team.name}>
+            {team.name}
+          </option>
+        ))}
+      </select>
+      {selectedTeam !== "" && (
+        <p>Chosen team: <strong>{selectedTeam}</strong></p>
       )}
-    </div>
+      </div>
   );
 }
 
